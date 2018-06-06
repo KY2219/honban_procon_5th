68b3e2c70cc272532adcb96ce3085a3f0222c3f6
山田 亘太 <98003@bsp.co.jp>
2018-06-06 12:55:32 +0900

    second commit

---
+++ Main2.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		if(num < 1 || num > Math.pow(10, 5)) {
+			return;
+		}
+		
+		
+		int i = 0;
+		//Scanner scan2 = new Scanner(System.in);
+		int[] out_x = new int[num];
+		
+		while(i < num) {
+			out_x[i] = scan.nextInt();
+			if(out_x[i] < -Math.pow(10,5) || out_x[i] > Math.pow(10,5))
+				return;
+			i++;
+		}
+		//ŒÂ”‚ª‚P‚Â‚Ì‚İ‚Ìê‡
+		if(num == 1){
+			System.out.println(1);
+			return;
+		}
+
+		Arrays.sort(out_x);
+
+		int point = num/2;
+		i = 0;
+		int count = 1;
+		while(i < out_x.length){
+			//•½‹Ï‚ğæ‚é•û–@i‚æ‚­‚È‚¢j
+			/*if((out_x[i+1] - out_x[i]) <= 2 && (out_x[i+1] - out_x[i]) >= 0){
+				out_x[i] = (out_x[i+1] + out_x[i])/2;
+				out_x[i+1] = (out_x[i+1] + out_x[i])/2;
+				count++;
+			}*/
+			if(out_x[i] < out_x[point]){
+				out_x[i]++;
+			}
+			if(out_x[i] > out_x[point]){
+				out_x[i]--;
+			}
+			i++;
+			
+			//if(i == out_x.length-1)
+			//	System.out.println(count);
+		}
+		
+		if((out_x[point]-out_x[point-1]) == 1){
+			out_x[point]--;
+		}
+		else if((out_x[point+1]-out_x[point]) == 1){
+			out_x[point]++;
+		}
+		
+		i=0;
+		while(i < out_x.length-1){
+			if(out_x[i] == out_x[i+1]){
+				count++;
+			}
+			i++;
+			
+			if(i == out_x.length-1)
+				System.out.println(count);
+		}
+
+	}
+
+}
---
+++ Main7.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+import java.io.*;
+
+public class Main7 {
+	public static void main(String[] args) {
+		//BufferedReader in = new BufferedReader (new InputStreamReader(System.in));
+		//try{
+			Scanner scan = new Scanner(System.in);
+			//String n[] = in.readLine().split("\\s+");
+			String n[] = scan.nextLine().split(" ");
+			//int dimen_num = scan.nextInt();
+			//if(!(n[0].equals(" ")))
+			//return;
+			
+			//サイトの入力例の謎の空白に対応（？）
+			int dimen_num = Integer.parseInt(n[0]);
+			if(dimen_num < 1 || dimen_num > 100) {
+				return;
+			}
+			//String str = scan.nextLine();
+			//if(!(str.equals(" ")))
+			//return;
+
+			//Scanner scan_P = new Scanner(System.in);
+			//String p[] = in.readLine().split("\\s+");
+			String p[] = scan.nextLine().split(" ");
+			int[] P = new int[dimen_num];
+			int i = 0;
+			while(i < dimen_num) {
+				//P[i] = scan.nextInt();
+				P[i] = Integer.parseInt(p[i]);
+				if(P[i] < -100000 || P[i] > 100000)
+					return;
+				i++;
+			}
+
+			//Scanner scan_Q = new Scanner(System.in);
+			//String q[] = in.readLine().split("\\s+");
+			String q[] = scan.nextLine().split("\\s+");
+			int[] Q = new int[dimen_num];
+			i = 0;
+			while(i < dimen_num) {
+				//Q[i] = scan.nextInt();
+				Q[i] = Integer.parseInt(q[i]);
+				if(Q[i] < -100000 || Q[i] > 100000)
+					return;
+				i++;
+			}
+			
+			double result = 0, sum = 0;
+			i = 0;
+			while(i < dimen_num){
+				sum += (P[i] - Q[i]) * (P[i] - Q[i]);
+				i++;
+			}
+			
+			result = Math.sqrt(sum);
+			BigDecimal bi = new BigDecimal(String.valueOf(result));
+			double result2 = bi.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+			if(result2 < 1 || result2 > 10000000)
+			return;
+			System.out.println(result2);
+		//}catch(Exception e){
+		//	System.out.println(e);
+		//}
+	return;
+	}
+
+}
